27f56614e5ae2b712f124fba8676cf28be113ebb
cherry <cherry@SONY-VAIO>
2017-09-11 12:59:35 +0530

    Verified Search Functionality.

--- src/gitHubPack/GitHubClass.java
+++ src/gitHubPack/GitHubClass.java
@@ -4,6 +4,7 @@ import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeTest;
@@ -13,6 +14,7 @@ public class GitHubClass {
 	@Test
 	public void google() {
 		driver.get("https://www.google.com/");
+		driver.findElement(By.name("q")).sendKeys("seleniumlearn.com");
 
 	}
 	@BeforeTest
